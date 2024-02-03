4cf3dd177ad42150586e97563a20b9ac787aac46
Vanessa Rodriguito <vabo.rodriguito.up@phinmaed.com>
2024-02-03 08:57:42 +0800

    initial commit

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 Code for app.js
-const hamburger = document.querySelector('.header .nav-bar .nav-list
-.hamburger');
+const hamburger = document.querySelector('.header .nav-bar .nav-list.hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul
-li a');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 hamburger.addEventListener('click', () => {
 hamburger.classList.toggle('active');
